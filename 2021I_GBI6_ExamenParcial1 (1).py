26c460f60f32823e4c137a61973897481e001c15
SamanthaGuinanzaca <94935304+SamanthaGuinanzaca@users.noreply.github.com>
2022-01-15 13:28:52 -0500

    Add files via upload
    Guiñanzaca Samantha ha resuelto el ejercicio 6 
    Guiñanzaca Samantha ha resuelto el ejercicio 7
    Guiñanzaca Samantha ha resuelto el ejercicio 8

---
+++ 2021I_GBI6_ExamenParcial1 (1).py
@@ -0,0 +1,277 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# <img src="https://www.ikiam.edu.ec/img/logo-ikiam-grey.png" width=300 height=240 />
+# 
+# # EXAMEN PARCIAL 1
+# 
+# ## GBI6-2021II: BIOINFORMÁTICA
+# 
+# **Guiñanzaca Vaca, Samantha Elizabeth** 
+# 
+# **14-01-2022**
+
+# <font color='red'>NO AGREGUE NUEVAS CELDAS, UTILICE ÚNICAMENTE LAS QUE ESTÁN CREADAS</font>
+
+# # 1. UNIX
+# 
+# ## Ejercicio 1
+# Para esta pregunta debe realizar previamente lo siguiente: 
+# 
+# 1. Imprimir el directorio de trabajo actual 
+# 2. Crear un directorio test1
+# 3. No cambie de directorio, pero los items 4, 5 y 6 deben guardarse en el directorio creado en el paso 2. 
+# 4. Crear el archivo test1.txt
+# 5. Guardar el texto Hola GBI6 en un archivo test2.txt
+# 6. Guardar la instrucción print("Hola GBI6")  en el archivo test1.py
+# 
+# Luego seleccione la instrucción que copie test2.txt al directorio test2/subtest1 y después borre el directorio test1.
+# 
+# <img src="data/Pregunta1.png" width=600 height=450 />
+# 
+# <img src="data/Continuacion 1 (2).png" width=600 height=450 />
+# 
+# <img src="data/Continuación P1.1.png" width=600 height=450 />
+# 
+# <img src="data/Continuación P1.2.png" width=600 height=450 />
+# 
+# Escriba aquí alguna explicación del ejercicio 1 si lo considera necesario (borre esta línea cuando finalice)
+# 
+# ## Ejercicio 2
+# Seleccione la opción de código que imprime el directorio de trabajo actual
+# 
+# <img src="data/Pregunta2.png" width=600 height=450 />
+# 
+# 
+# ## Ejercicio 3
+# 
+# Para esta pregunta debe realizar previamente lo siguiente: 
+# 
+# 1. Imprimir el directorio de trabajo actual 
+# 2. Crear un directorio test1
+# 3. No cambie de directorio, pero los items 4, 5 y 6 deben guardarse en el directorio creado en el paso 2. 
+# 4. Crear el archivo test1.txt
+# 5. Guardar el texto Hola GBI6 en un archivo test2.txt
+# 6. Guardar la instrucción print("Hola GBI6")  en el archivo test1.py
+# 
+# Luego seleccione la instrucción que permite enlistar el contenido (sin detalles) del directorio test1
+# 
+# 
+# <img src="data/Pregunta3 (2).png" width=600 height=450 />
+# 
+# <img src="data/Continuación 3.png" width=600 height=450 />
+# 
+# 
+# Escriba aquí alguna explicación del ejercicio 3 si lo considera necesario (borre esta línea cuando finalice)
+# 
+# ## Ejercicio 4
+# Resuelva el ejercicio 1.10.4 Data Explorer. 
+# 
+# Resuelva el ejercicio en un documento de nombre ejercicio_1104.sh
+# Cargue el documento ejercicio_1104.sh en GitHub utilizando git push. 
+# Realice el video explicativo de la resolución ejercicio y guarde en YouTube en formato "Público" o "Unlisted", y pegue el enlace en el cuaderno de Jupyter.
+# 
+
+# In[4]:
+
+
+# Coloque aquí su video del ejercicio 4
+from IPython.display import HTML
+HTML('<iframe width="560" height="315" src="https://www.youtube.com/embed/jG_CSBwI3cw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>')
+
+
+# 
+# 
+# ## Ejercicio 5
+# Resuelva el ejercicio 1.10.1 Next Generation Sequencing Data. 
+# 
+# Resuelva el ejercicio en un documento de nombre ejercicio_1101.sh
+# Cargue el documento ejercicio_1101.sh en GitHub utilizando git push. 
+# Realice el video explicativo de la resolución ejercicio y guarde en YouTube en formato "Público" o "Unlisted", y pegue el enlace en el cuaderno de Jupyter.
+# 
+
+# In[3]:
+
+
+# Coloque aquí su video del ejercicio 5
+from IPython.display import HTML
+HTML('<iframe width="560" height="315" src="https://www.youtube.com/embed/eLTLyaHIuPI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>')
+
+
+# Escriba aquí alguna explicación del ejercicio 5 si lo considera necesario (borre esta línea cuando finalice)
+# 
+# # 2. CONTROL DE LA VERSIÓN
+# ## Pregunta 6
+# 1. Cree en GitHub un repositorio de nombre ```GBI6_ExamenParcial1```. 
+# 2. Cree un archivo ```Readme.md``` que debe tener lo siguiente: 
+# - Datos personales
+# - Características del computador
+# - Programas utilizados
+# - Explicación de la data utilizada
+# 
+# 
+# ## Pregunta 7
+# 1. Cada vez que resuelva un ejercicio (del 1 al 10), utilizando Git Bash, GitHub Desktop o Atom realice un *git push* con el mensaje: 
+# 
+# 
+# <font color='red'>Apellido Nombre ha resuelto el ejercicio 1</font> % Cambie con sus apellidos y nombres; y con el número de ejercicio que corresponda 
+# 
+# 2. Coloque aquí las capturas de pantalla para cada git push realizado desde Git Bash, GitHub Desktop o Atom. 
+# 
+# - Control de versión ejercicio 1
+# 
+# <img src="Pregunta1CV.png" width=600 height=450 />
+# 
+# - Control de versión ejercicio 2
+# 
+# <img src="Pregunta2CV.png" width=600 height=450 />
+# 
+# - Control de versión ejercicio 3
+# 
+# <img src="data/ejemplo_unix.jpg" width=600 height=450 />
+# 
+# - Control de versión ejercicio 4
+# 
+# <img src="Pregunta4CV.png" width=600 height=450 />
+# 
+# - Control de versión ejercicio 5
+# 
+# <img src="data/ejemplo_unix.jpg" width=600 height=450 />
+# 
+# - Control de versión ejercicio 6
+# 
+# <img src="data/ejemplo_unix.jpg" width=600 height=450 />
+# 
+# - Control de versión ejercicio 7
+# 
+# <img src="data/ejemplo_unix.jpg" width=600 height=450 />
+# 
+# - Control de versión ejercicio 8
+# 
+# <img src="data/ejemplo_unix.jpg" width=600 height=450 />
+# 
+# - Control de versión ejercicio 9
+# 
+# <img src="data/ejemplo_unix.jpg" width=600 height=450 />
+# 
+# - Control de versión ejercicio 10
+# 
+# <img src="data/ejemplo_unix.jpg" width=600 height=450 />
+# 
+# 
+# 3. Cree un directorio de nombre ```data``` donde deben estar guardados: (i) la data utilizada y (ii) las capturas de pantalla de los ejercicios 1 al 3 y los git push del ejercicio 1 al 10 (en total debe tener 13 imágenes). 
+# 4. Inserte aquí el enlace de su repositorio (válido para el ejercicio 6 y 7): [GitHub Repository: GBI6_ExamenParcial1](https://github.com/gualapuromoises/2021_gbi6.git) (este enlace es de ejemplo)
+
+# # 3. INTRODUCCIÓN A PYTHON
+# 
+# ## Ejercicio 8
+# 
+# Para este ejercicio tiene que crear previamente lo siguiente: 
+# 
+# En la data https://github.com/sap0408/mtcars/blob/master/mtcars.csv, seleccione los 10 primeros registros y cree una lista de nombre lista1.
+# 
+# Relaciones los códigos y las respuestas que salen al ejecutar. 
+# 
+
+# In[26]:
+
+
+import pandas as pd
+mzd = ['Mazda RX4', 21, 6, 160, 110, 3.9, 2.62, 16.46, 0, 1, 4, 4]
+mzda = ['Mazda RX4 Wag', 21, 6, 160, 110, 3.9, 2.875, 17.02, 0, 1, 4, 4]
+dts = ['Datsun 710', 22.8, 4, 108, 93, 3.85, 2.32, 18.61, 1, 1, 4, 1]  
+hrt = ['Hornet 4 Drive', 21.4, 6, 258, 110, 3.08, 3.215, 19.44, 1, 0, 3, 1] 
+hret = ['Hornet Sportabout', 18.7, 8, 360, 175, 3.15, 3.44, 17.02, 0, 0, 3, 2] 
+val = ['Valiant', 18.1, 6, 225, 105, 2.76, 3.46, 20.22, 1, 0, 3, 1]
+dst = ['Duster', 14.3, 8, 360, 245, 3.21, 3.57, 15.84, 0, 0, 3, 4]
+mer = ['Merc 240D', 24.4, 4, 146.7, 62, 3.69, 3.19, 20, 1, 0, 4, 2] 
+merc = ['Merc 230', 22.8, 4, 140.8, 95, 3.92, 3.150, 22.90, 1, 0, 4, 2] 
+mrc = ['Merc 280', 19.2, 6, 167.6, 123, 3.92, 3.440, 18.30, 1, 0, 4, 4] 
+
+
+lista1=lista1_rrss = [mzd, mzda, dts, hrt, hret, val, dst, mer, merc, mrc] 
+df_rrss=pd.DataFrame(lista1_rrss, 
+                     columns = ['model', 'mpg', 'cyl', 'disp', 'hp', 'drat', 'wt', 'qsec', 'vs', 'am', 'gear', 'carb']) 
+df_rrss
+
+opcion1=lista1[3]  
+opcion2=print(len(lista1))
+lista1.append(["gbi6", "bio1", "ikiam"])
+print(lista1)
+lista1[4]
+lista1[2:5]
+lista1[-1][-3]
+lista1 = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "ll", "m", "n", "ñ", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+len(lista1)
+
+
+# ## Ejercicio 9
+# 
+# Para este ejercicio debe debe realizar lo siguiente:
+# 
+# 1. Crear un diccionario de nombre dic_genomas, donde se tenga la información de la tabla 1 que se encuentra en https://www.nature.com/scitable/topicpage/eukaryotic-genome-complexity-437/. 
+# 2. Utilice como "key" los valores especie de la columna "Species and Common Name" y las otras dos columnas ingrese como "values". 
+# 3. Luego de crear el diccionario, extraiga los valores de "key" a una lista de nombre "especies"
+# 4. Extraiga los valores de "value" a una lista de nombre "genomas"
+# 
+# Para evaluar esta pregunta realice un git push del Jupyter Notebook al repositorio del examen. 
+
+# In[16]:
+
+
+dic_genomes={"Saccharomyces cerevisiae (unicellular budding yeast)": ["12 million", 6000], 
+              "Trichomonas vaginalis ": ["160 million", 60000], 
+              "Plasmodium falciparum (unicellular malaria parasite)": ["23 million", 5000], 
+              "Caenorhabditis elegans (nematode)": ["95.5 million", 18000],
+              "Drosophila melanogaster (fruit fly)": ["170 million", 14000],
+              "Arabidopsis thaliana (mustard; thale cress)": ["125 million", 25000],
+              "Oryza sativa (rice)": ["470 million", 51000],
+              "Gallus gallus (chicken)": ["1 billion", 20000-23000],
+              "Canis familiaris (domestic dog)": ["2.4 billion", 19000],
+              "Mus musculus (laboratory mouse)": ["2.5 billion", 30000],
+              "Homo sapiens (human)": ["2.9 billion", 20000-25000]}
+dic_genomes.items()
+keys = []
+values = []
+
+ #Accediendo al valor de cada uno de las partes del diccionario y valores para redifinirlos en una lista
+for (key, val) in dic_genomes.items():
+    keys.append(key)
+    values.append(val)
+
+#Se redifine la variable keys e otra ahora denominada especies, que será una lista
+especies=keys
+
+#Se redifine la varibale values e otra ahora denominada genomas, que será una lista
+genomas=values
+
+
+# ## Ejercicio 10
+# 
+# Para este ejercicio tiene que crear previamente lo siguiente: 
+# 
+# -Una lista de nombre lista1 que tenga a las 10 primeras letras del alfabeto.
+# 
+# Relaciones los códigos y las respuestas que salen al ejecutar. 
+
+# In[68]:
+
+
+lista1 = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "ll", "m", "n", "ñ", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+lista1.append(["gbi6", "bio1", "ikiam"])
+print(lista1)
+
+print(lista1[-1][-1])
+
+print(lista1[2:5])
+
+print(lista1[3])
+
+lista1.append(["gbi6", "bio1", "ikiam"])
+print(lista1)
+print(len(lista1))
+
+
+
